748392a1f38bacb42a432ebf2acb2d2dc7b02e52
Tijmen Kattenberg <tkattenberg@live.nl>
2022-03-22 13:13:14 +0100

    Added GridSetup

--- src/main.f90
+++ src/main.f90
@@ -1,3 +1,18 @@
 program main
+    use GridSetup
+    implicit none
+    real, allocatable :: grid(:)
+    real :: Length, LowerBound, UpperBound
+    integer :: GridPoints
+    
+    Length = 2.
+    GridPoints = 20
+    allocate(grid(GridPoints))
+
+    LowerBound = -Length/2.
+    UpperBound = Length/2.
+
+    grid = CreateGrid(GridPoints,LowerBound,UpperBound)
+
     
 end program main
